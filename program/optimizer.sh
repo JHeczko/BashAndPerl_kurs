c7f82ca1eb441c3dcb0f2c1494cd0d196abd6874
wrex <jakub.heczko5@gmail.com>
2025-10-21 11:03:08 +0200

    parsing working, now implementing logic

--- program/optimizer.sh
+++ program/optimizer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Jakub Heczko
 
-# check_for_help_apperance()
-# checkes if argument -help is in the params
-#
-# Input Arguments:
-# - all program input parameters
+HARDLINKS_FLAG=1
+MAX_DEPTH=0
+HASH_ALGO="md5sum"
+
+
 function check_for_help_apperance(){
   for arg in "$@"; do
     if [[ $arg == "--help" ]]; then
@@ -15,39 +15,53 @@ function check_for_help_apperance(){
   done
 }
 
-check_for_help_apperance "$@"
-
+function parse_args(){
 ARGS=$(getopt -o "" -l "replace-with-hardlinks,max-depth:,hash-algo:,help" -- "$@")
+if [[ $? -ne 0 ]]; then
+  echo "[ERROR] Failed to parse arguments"
+  exit 1
+fi
 
 eval set -- "$ARGS"
 
-for arg in "$@"; do
-  case "$arg" in
+while true; do
+  case "$1" in
+
     --replace-with-hardlinks)
+
+      HARDLINKS_FLAG=0
       shift
       ;;
 
 
     --max-depth)
       if [[ -z "$2" || "$2" == -* ]]; then
-        echo "--max-depth option require a number of the recursion depth"
+        echo "[ERROR] --max-depth option require a posotive number of the recursion depth"
         exit 1
       fi
 
       if [[ ! "$2" =~ [0-9]+ ]]; then
-        echo "Opcja musi byc integerem"
+        echo "[ERROR] Opcja musi byc integerem"
         exit 1
       fi
 
+      MAX_DEPTH=$2
       shift 2
       ;;
 
 
     --hash-algo)
      if [[ -z "$2" || "$2" == -* ]]; then
-        echo "--hash-algo option require a name of algorithm"
+        echo "[ERROR] --hash-algo option require a name of algorithm"
         exit 1
       fi
+
+      if [[ "$2" != "md5sum" && "$2" != "sha1sum" && "$2" != "sha256sum" ]]; then
+        echo "[ERROR] No such hashing algorythm supported, please choose only from the following list: md5sum ,sha1sum ,sha256sum"
+        exit 1
+      fi
+
+      HASH_ALGO="$2"
       shift 2
       ;;
 
@@ -59,10 +73,38 @@ for arg in "$@"; do
 
 
     *)
-      echo "Nieznana opcja: $1"
+      echo "[ERROR] Nieznana opcja: $1"
       exit 1
       ;;
 
-
   esac
 done
+}
+
+function hash(){
+  local text=$1
+  echo -n "$text" | $HASH_ALGO | awk '{print $1}'
+}
+
+function indepth_search(){
+  files=$@
+  files_depth_down=""
+
+  for file in $files; do
+    if [[ $file == '.' || $file == '..' ]]; then
+      continue
+    fi
+
+    if [[ -d file ]]
+
+    echo $(len file)
+  done
+}
+
+# =================================================
+
+check_for_help_apperance "$@"
+
+parse_args "$@"
+
+indepth_search $(ls -a)
